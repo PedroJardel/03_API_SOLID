8a193c56371fb9933b98a6beb77f3f117c8600fc
Pedro Lima <pedrozagolima@gmail.com>
2024-11-12 17:52:16 -0300

    fix: implement dependency inversion principle

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -8,28 +8,25 @@ interface RegisteruseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisteruseCaseRequest) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
+  async execute({ name, email, password }: RegisteruseCaseRequest) {
+    const password_hash = await hash(password, 6)
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exixts.')
+    }
+
+    await this.usersRepository.create({
+      name,
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exixts.')
+      password_hash,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
